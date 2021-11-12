92bb4260d71d9ac9649108eb06525f02157ecb8a
takker99 <37929109+takker99@users.noreply.github.com>
2021-11-13 08:08:35 +0900

    :+1: Support for all unicode charcters

--- mod.ts
+++ mod.ts
@@ -1,23 +1,19 @@
 const INITPAT = 0x80000000;
-const MAXCHAR = 0x10000;
 const INITSTATE = [INITPAT, 0, 0, 0];
-const isupper = (c: number) => (c >= 0x41) && (c <= 0x5a);
-const islower = (c: number) => (c >= 0x61) && (c <= 0x7a);
-const tolower = (c: number) => isupper(c) ? (c + 0x20) : c;
-const toupper = (c: number) => islower(c) ? (c - 0x20) : c;
-const wildCard = 0x20;
+const wildCard = " ";
 
 export function Asearch(source: string) {
-  const shiftpat = new Uint32Array(MAXCHAR);
+  const shiftpat = new Map<string, number>();
   let epsilon = 0;
   let mask = INITPAT;
-  for (const i of unpack(source)) {
-    if (i === wildCard) {
+  for (const char of source) {
+    if (char === wildCard) {
       epsilon |= mask;
     } else {
-      shiftpat[i] |= mask;
-      shiftpat[toupper(i)] |= mask;
-      shiftpat[tolower(i)] |= mask;
+      for (const i of [char, char.toLowerCase(), char.toUpperCase()]) {
+        const pat = (shiftpat.get(i) ?? 0) | mask;
+        shiftpat.set(i, pat);
+      }
       mask >>>= 1;
     }
   }
@@ -25,15 +21,15 @@ export function Asearch(source: string) {
 
   function getState(str = "") {
     let [i0, i1, i2, i3] = INITSTATE;
-    for (const c of unpack(str)) {
-      mask = shiftpat[c];
+    for (const char of str) {
+      mask = shiftpat.get(char) ?? 0;
       i3 = (i3 & epsilon) | ((i3 & mask) >>> 1) | (i2 >>> 1) | i2;
       i2 = (i2 & epsilon) | ((i2 & mask) >>> 1) | (i1 >>> 1) | i1;
       i1 = (i1 & epsilon) | ((i1 & mask) >>> 1) | (i0 >>> 1) | i0;
       i0 = (i0 & epsilon) | ((i0 & mask) >>> 1);
-      i1 |= (i0 >>> 1);
-      i2 |= (i1 >>> 1);
-      i3 |= (i2 >>> 1);
+      i1 |= i0 >>> 1;
+      i2 |= i1 >>> 1;
+      i3 |= i2 >>> 1;
     }
     return [i0, i1, i2, i3];
   }
@@ -57,10 +53,4 @@ export function Asearch(source: string) {
     match,
     source,
   };
-}
-
-function* unpack(str: string) {
-  for (const char of str) {
-    yield char.charCodeAt(0);
-  }
 }
