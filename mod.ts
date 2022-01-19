3a6e69982421f3eb91c0c4a50e25c1f3267d7bca
takker99 <37929109+takker99@users.noreply.github.com>
2022-01-19 14:18:42 +0900

    :sparkles: 途中の計算状態を取得できるようにした

--- mod.ts
+++ mod.ts
@@ -1,55 +1,121 @@
-const INITPAT = 0x80000000;
-const INITSTATE = [INITPAT, 0, 0, 0];
+const INITPATTERN = 0x80000000;
+export const INITSTATE = [INITPATTERN, 0, 0, 0] as State;
 const wildCard = " ";
 
-export function Asearch(source: string) {
-  const shiftpat = new Map<string, number>();
-  let epsilon = 0;
-  let mask = INITPAT;
+/** あいまい検索に使うマスクを作成する
+ *
+ * @param source 検索対象の文字列
+ */
+export function preparePatterns(source: string) {
+  const shiftPattern = new Map<string, number>();
+  let epsilonPattern = 0;
+  let acceptPattern = INITPATTERN;
   for (const char of source) {
     if (char === wildCard) {
-      epsilon |= mask;
+      epsilonPattern |= acceptPattern;
     } else {
       for (const i of [char, char.toLowerCase(), char.toUpperCase()]) {
-        const pat = (shiftpat.get(i) ?? 0) | mask;
-        shiftpat.set(i, pat);
+        const pat = (shiftPattern.get(i) ?? 0) | acceptPattern;
+        shiftPattern.set(i, pat);
       }
-      mask >>>= 1;
+      acceptPattern >>>= 1;
     }
   }
-  const acceptpat = mask;
 
-  function getState(str = "") {
-    let [i0, i1, i2, i3] = INITSTATE;
-    for (const char of str) {
-      mask = shiftpat.get(char) ?? 0;
-      i3 = (i3 & epsilon) | ((i3 & mask) >>> 1) | (i2 >>> 1) | i2;
-      i2 = (i2 & epsilon) | ((i2 & mask) >>> 1) | (i1 >>> 1) | i1;
-      i1 = (i1 & epsilon) | ((i1 & mask) >>> 1) | (i0 >>> 1) | i0;
-      i0 = (i0 & epsilon) | ((i0 & mask) >>> 1);
-      i1 |= i0 >>> 1;
-      i2 |= i1 >>> 1;
-      i3 |= i2 >>> 1;
-    }
-    return [i0, i1, i2, i3];
+  return [shiftPattern, epsilonPattern, acceptPattern] as const;
+}
+
+export type State = [number, number, number, number];
+/** 状態遷移機械に文字列を入力する
+ *
+ * @param text 入力する文字列
+ * @param shiftPattern 遷移可能な場所を示したビットパタンのテーブル
+ * @param epsilonPattern 任意文字遷移が可能な場所を示したビットパタン
+ * @param 入力前の状態遷移機械
+ */
+export function moveState(
+  text: string,
+  shiftPattern: Map<string, number>,
+  epsilonPattern: number,
+  prevState = INITSTATE,
+): State {
+  let [i0, i1, i2, i3] = prevState;
+  for (const char of text) {
+    const mask = shiftPattern.get(char) ?? 0;
+    i3 = (i3 & epsilonPattern) | ((i3 & mask) >>> 1) | (i2 >>> 1) | i2;
+    i2 = (i2 & epsilonPattern) | ((i2 & mask) >>> 1) | (i1 >>> 1) | i1;
+    i1 = (i1 & epsilonPattern) | ((i1 & mask) >>> 1) | (i0 >>> 1) | i0;
+    i0 = (i0 & epsilonPattern) | ((i0 & mask) >>> 1);
+    i1 |= i0 >>> 1;
+    i2 |= i1 >>> 1;
+    i3 |= i2 >>> 1;
   }
+  return [i0, i1, i2, i3];
+}
 
-  function test(str: string, distance = 0) {
-    const state = getState(str);
-    distance = Math.min(INITSTATE.length - 1, distance);
-    return (state[distance] & acceptpat) !== 0;
+/** あいまい検索結果
+ *
+ * 見つかった場合はLevenshtein距離も格納する
+ */
+export type MatchResult = {
+  found: false;
+} | {
+  found: true;
+  distance: 0 | 1 | 2 | 3;
+};
+export interface AsearchResult {
+  /** 検索対象の文字列 */ source: string;
+
+  /** 与えた文字列がLevenshtein距離`maxDistance`以下でマッチするか判定する
+   *
+   * @param str 判定する文字列
+   * @param maxDistance 許容する最大Levenshtein距離
+   */
+  test: (str: string, maxDistance: 0 | 1 | 2 | 3) => boolean;
+
+  /** 与えた文字列と検索対象文字列とのLevenshtein距離を返す
+   *
+   * Levenshtein距離が4以上の場合はマッチしなかったとみなす
+   *
+   * @param str この文字列との距離を計算する
+   */
+  match: (str: string) => MatchResult;
+}
+/** あいまい検索を実行する函数を作る
+ *
+ * @param source 検索対象の文字列
+ */
+export function Asearch(source: string): AsearchResult {
+  const [shiftPattern, epsilonPattern, acceptPattern] = preparePatterns(source);
+
+  function test(str: string, maxDistance: 0 | 1 | 2 | 3 = 0) {
+    const state = moveState(str, shiftPattern, epsilonPattern);
+    return (state[maxDistance] & acceptPattern) !== 0;
   }
 
   function match(
     str: string,
-  ): { found: false } | { found: true; distance: number } {
-    const state = getState(str);
-    if ((state[INITSTATE.length - 1] & acceptpat) === 0) {
+  ): MatchResult {
+    const [state0, state1, state2, state3] = moveState(
+      str,
+      shiftPattern,
+      epsilonPattern,
+    );
+    if ((state3 & acceptPattern) === 0) {
       return { found: false };
     }
-    const distance = state.findIndex((i) => (i & acceptpat) !== 0);
-    return { found: true, distance };
+    return {
+      found: true,
+      distance: (state0 & acceptPattern) !== 0
+        ? 0
+        : (state1 & acceptPattern) !== 0
+        ? 1
+        : (state2 & acceptPattern) !== 0
+        ? 2
+        : 3,
+    };
   }
+
   return {
     test,
     match,
